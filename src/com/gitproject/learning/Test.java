98cd34dba999aa4a2ace9b4492b4672d3223d5ba
bhanu <bhanu@DESKTOP-960L3EI>
2022-10-22 23:43:38 +0530

    first commit...

---
+++ src/com/gitproject/learning/Test.java
@@ -0,0 +1,14 @@
+package com.gitproject.learning;
+
+public class Test {
+
+	public static void main(String[] args) {
+	String a ="bhanu";
+	String b= "bhanu";
+	if(a.equalsIgnoreCase(b)) {
+		System.out.println("pass");
+	}else
+        System.out.println("fail");
+	}
+
+}
