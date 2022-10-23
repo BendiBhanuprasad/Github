3a87861c28e23c6cb5096e5fae074de9293126cc
bhanu <bhanu@DESKTOP-960L3EI>
2022-10-23 22:58:03 +0530

    commiting test2 to feature branch

---
+++ src/com/gitproject/learning/Test2.java
@@ -0,0 +1,12 @@
+package com.gitproject.learning;
+
+public class Test2 {
+	public static void main(String[] args) {
+		String a ="bhanu";
+		String b= "prasad";
+		if(a.equalsIgnoreCase(b)) {
+			System.out.println("pass");
+		}else
+	        System.out.println("fail");
+		}
+}
